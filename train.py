870be261ef9a405e4a442b3adf461cd42e338694
ZhixuanLiu <38152395+ZhixuanLiu@users.noreply.github.com>
2020-06-11 16:54:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -52,7 +52,8 @@ def fit_ont_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
         outputs = net(images)
         losses = []
         for i in range(3):
-            print( outputs.shape, targets.shape )
+            for o_j, t_j in zip(outputs, targets):
+                print( o_j.shape, t_j.shape )
             loss_item = yolo_losses[i](outputs[i], targets)
             losses.append(loss_item[0])
         loss = sum(losses)
