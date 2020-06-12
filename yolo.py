561bb1799bef1201740935bac73145bd2d73cdd6
ZhixuanLiu <38152395+ZhixuanLiu@users.noreply.github.com>
2020-06-12 16:49:54 +0800

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -179,4 +179,44 @@ class YOLO(object):
             draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
             del draw
         return image
+    
+    def get_boxes(self, image):
+        image_shape = np.array(np.shape(image)[0:2])
+
+        crop_img = np.array(letterbox_image(image, (self.model_image_size[0],self.model_image_size[1])))
+        photo = np.array(crop_img,dtype = np.float32)
+        photo /= 255.0
+        photo = np.transpose(photo, (2, 0, 1))
+        photo = photo.astype(np.float32)
+        images = []
+        images.append(photo)
+        images = np.asarray(images)
+
+        with torch.no_grad():
+            images = torch.from_numpy(images)
+            if self.cuda:
+                images = images.cuda()
+            outputs = self.net(images)
+            
+        output_list = []
+        for i in range(3):
+            output_list.append(self.yolo_decodes[i](outputs[i]))
+        output = torch.cat(output_list, 1)
+        batch_detections = non_max_suppression(output, len(self.class_names),
+                                                conf_thres=self.confidence,
+                                                nms_thres=0.3)
+        try:
+            batch_detections = batch_detections[0].cpu().numpy()
+        except:
+            return image
+            
+        top_index = batch_detections[:,4]*batch_detections[:,5] > self.confidence
+        top_conf = batch_detections[top_index,4]*batch_detections[top_index,5]
+        top_label = np.array(batch_detections[top_index,-1],np.int32)
+        top_bboxes = np.array(batch_detections[top_index,:4])
+        top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(top_bboxes[:,0],-1),np.expand_dims(top_bboxes[:,1],-1),np.expand_dims(top_bboxes[:,2],-1),np.expand_dims(top_bboxes[:,3],-1)
+
+        # 去掉灰条
+        boxes = yolo_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.model_image_size[0],self.model_image_size[1]]),image_shape)
+        return boxes
 
