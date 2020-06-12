b1508596ed1fa20a2ed66daddde4c7786145d3ae
ZhixuanLiu <38152395+ZhixuanLiu@users.noreply.github.com>
2020-06-12 14:54:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,14 @@ from nets.yolo4 import YoloBody
 
 if __name__ == "__main__":
     # 需要使用device来指定网络在GPU还是CPU运行
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = YoloBody(3,20).to(device)
+    if True: 
+        model_path = './wheat_yolo.pth'
+        print('Loading weights into state dict...')
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model_dict = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location=device)
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) ==  np.shape(v)}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
     summary(model, input_size=(3, 416, 416))
